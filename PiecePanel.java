cc7f62732b6b4ddc9ce1a43a7f2fc6e553b2a72f
bbirney <ben.birney@gmail.com>
2015-02-18 13:10:50 -0500

    Update PiecePanel.java

--- PiecePanel.java
+++ PiecePanel.java
@@ -1,1 +1,33 @@
-//
+import java.awt.GridLayout;
+import java.awt.Dimension;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JPanel;
+import javax.swing.Box;
+import javax.swing.JLabel;
+
+public class PiecePanel extends JPanel
+{
+  private Piece p1;
+  private Piece p2;
+  private Piece p3;
+
+  PiecePanel(Piece piece1, Piece piece2, Piece piece3)
+  {
+    p1 = piece1.setRandomPiece();
+    p2 = piece2.setRandomPiece();
+    p3 = piece3.setRandomPiece();
+    Box box = Box.createHorizontalBox();
+    box.setPreferredSize(new Dimension(130, 300));
+    JPanel pa1 = new JPanel();
+    pa1.setLayout(new GridLayout(17,5,10,10));
+    int length1=p1.getLength();
+    int length2=p2.getLength();
+    int length3=p3.getLength();
+    pa1.add(p1);
+    pa1.add(p2);
+    pa1.add(p3);
+  }
+}
