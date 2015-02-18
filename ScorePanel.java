ef26a4bc470b338a0c0a2339c723a237d35c94b9
Yonghyun Lee <ynlee16@choate.edu>
2015-02-18 13:39:29 -0500

    Update ScorePanel.java

--- ScorePanel.java
+++ ScorePanel.java
@@ -1,9 +1,12 @@
+import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridLayout;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.Box;
