eb5dc9f4d2cbcffcd213b8e9f3d6e165e9b2e4e6
seliason <seliason15@choate.edu>
2015-02-23 11:36:30 -0500

    Update ScorePanel.java

--- ScorePanel.java
+++ ScorePanel.java
@@ -1,9 +1,12 @@
 import java.awt.Font;
-
+import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.Box;
@@ -18,7 +21,7 @@ public class ScorePanel extends JPanel
  TenTenCharMatrix tenTenCharMatrix = new TenTenCharMatrix(10,10); //make a TenTenCharMatrix class variable
  Piece piece = new Piece();
  PiecePanel piecePanel = new PiecePanel();
- private LetterPanel scorekeeper;
+ private JTextField scorekeeper;
  private JButton newGame;
  private JButton help;
  
@@ -74,66 +77,63 @@ public class ScorePanel extends JPanel
   score += piece.blockArea(piece.pieceShape);
  }
  
- public ScorePanel(Letterpanel lpanel) //constructor
+ public ScorePanel() //constructor
  	{
- 	scorekeeper = lpanel;
  	Box box = Box.createVerticalBox();
  	box.setPreferredSize(new Dimension(130,300));
  	JPanel p1 = new JPanel();
  	p1.setLayout(new GridLayout(2,2,10,10));
  	newGame = new JButton("New Game");
- 	newGame.addActionListener(new NewGameListener);
+ 	newGame.addActionListener(new NewGameListener());
  	p1.add(newGame);
  	help = new JButton("Help");
- 	help.addActionListener(new HelpListener);
+ 	help.addActionListener(new HelpListener());
  	p1.add(new JLabel("Score:",null, JLabel.RIGHT));
-	scoreField = new JTextField("100");
-   	scoreField.setEditable(false);
+	scorekeeper = new JTextField("100");
+   	scorekeeper.setEditable(false);
  	box.add(p1);
 	this.add(box); 	
  	}
 	 
-public NewGameListener implements ActionListener
-{
-	public void actionPerformed(ActionEvent e)
-	{
-		score = 0;
-	   	tenTenCharMatrix.clear();
-	    	piecePanel.new();
-	}
+//public NewGameListener implements ActionListener
+//{
+//	public void actionPerformed(ActionEvent e)
+//	{
+//		score = 0;
+//	   	tenTenCharMatrix.clear();
+//	    	piecePanel.new();
+//	}
+//}
+
+public void newGame(){
+	
+	score = 0;
+	apple.clear();
+	apple3.new();
 }
 
-public HelpListener implements ActionListener
-{
-	public void actionPerformed(ActionEvent e)
-	{
-	JOptionPane.showMessageDialog(null,
-        "The goal is to drop blocks in order to create" +
-        " and destroy full lines on the screen both" +
-        " vertically and horizontally.\n" +
-        "Don't forget to keep the blocks from filling the screen.\n" +
-        "Use the mouse to select pieces and place them in the grid.\n" +
-        "No time limit, no colour match!\n" +
-        " Fill all the grids with the matching blocks and enjoy 1010!" +
-        "Scoring:\n\n" +
-        "  Complete a Row: 10 pts" +
-        "Placing Blocks: \n\n" +
-        " 1 block piece: 1 pt --- 2 block piece: 2 pts " +
-        " 3 block piece: 3 pts --- 4 block piece: 4 pts --- etc. ",
-        "How to Play",       // Dialog title
-        JOptionPane.PLAIN_MESSAGE);
-	}
-}
+//public HelpListener implements ActionListener
+//{
+//	public void actionPerformed(ActionEvent e)
+//	{
+//	JOptionPane.showMessageDialog(null,
+  //      "The goal is to drop blocks in order to create" +
+    //    " and destroy full lines on the screen both" +
+      //  " vertically and horizontally.\n" +
+        //"Don't forget to keep the blocks from filling the screen.\n" +
+        //"Use the mouse to select pieces and place them in the grid.\n" +
+        //"No time limit, no colour match!\n" +
+//        " Fill all the grids with the matching blocks and enjoy 1010!" +
+  //      "Scoring:\n\n" +
+    //    "  Complete a Row: 10 pts" +
+      //  "Placing Blocks: \n\n" +
+        //" 1 block piece: 1 pt --- 2 block piece: 2 pts " +
+  //      " 3 block piece: 3 pts --- 4 block piece: 4 pts --- etc. ",
+    //    "How to Play",       // Dialog title
+      //  JOptionPane.PLAIN_MESSAGE);
+//	}
+//}
 
-public void actionPerformed(ActionEvent e)
-  {
-    JMenuItem src = (JMenuItem)e.getSource(); //cast as a menu item
-	if (src == exit)
-    {
-      System.exit(0); 
-
-    }
-  }
 	  
 }
 }
