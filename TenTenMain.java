ceb89df88edc32f371c5a3b0cd9ed0fc2fd5b890
seliason <seliason15@choate.edu>
2015-02-26 21:07:30 -0500

    Update TenTenMain.java

--- TenTenMain.java
+++ TenTenMain.java
@@ -1,28 +1,33 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.Box;
 
-public class TenTen extends JFrame
+public class TenTen extends JPanel implements ScoreDisplay
 {
  
 private PiecePanel piecePanel;
 private ScorePanel scorePanel;
-  
+
+
 public TenTen()
 {
-  LetterPanel scorekeeper = new LetterPanel(this);
-  piecepanel = new PiecePanel();
-  whiteboard.setScoreDisplay(piecepanel);
-  piecepanel.addMouseListener(new GameMouseListener());
+  ScorePanel scorekeeper = new ScorePanel();
+  piecePanel = new PiecePanel();
+  scorekeeper.setScoreDisplay(piecePanel);
+  piecePanel.addMouseListener(new GameMouseListener());
+  scorePanel.addMouseListener(new GameMouseListener());
   
   Box box = Box.createVerticalBox();
-    box.add(scorekeeper);
     box.add(scorePanel);
+    //add in grid.  
+    box.add(piecePanel);
     getContentPane().add(box);
 
     newGame();
   
 }
+
 
 public void newGame()
   {
@@ -40,12 +45,18 @@ public static void main(String[] args)
        // handle exception
     }
          
-    new SwingApplication(); //Create and show the GUI. 
-    TenTen window = new TenTen();
-    window.setBounds(100, 100, 395, 355);
-    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    window.setVisible(true);
+    //Create and show the GUI. 
+    TenTen game = new TenTen();
+    game.setBounds(100, 100, 395, 355);
+    game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    game.setVisible(true);
   }
+
+@Override
+public void update(int score) {
+	// TODO Auto-generated method stub
+	
+}
   
   
 }
